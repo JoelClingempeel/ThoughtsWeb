bd99bee2716a71ccf239fe64650cbd00d6ed85d8
Joel Clingempeel <joel.clingempeel@gmail.com>
2020-10-28 19:54:42 -0400

    Made graph directed plus minor refactoring.

--- static/graph.js
+++ static/graph.js
@@ -1,10 +1,10 @@
-const url = 'http://' + window.location.href.split('/')[2] + '/'
+const url = 'http://127.0.0.1:5000/'
 
 var data = {
     nodes: new vis.DataSet([]),
     edges: new vis.DataSet([])
 };
-var network = new vis.Network(document.getElementById('mynetwork'), data, {});
+var network = new vis.Network(document.getElementById('network_panel'), data, {});
 var selectedNode = ''
 var selectedEdge = ''
 var node_list;
@@ -27,10 +27,10 @@ async function call_api(query_type, query_data) {
 
 async function update_display_text(note_label) {
     if (note_label == '') {
-        document.getElementById('paragraph_display').innerHTML = '';
+        document.getElementById('note_display').innerHTML = '';
     } else {
         let display_text = await call_api('get_note', { node: note_label });
-        document.getElementById('paragraph_display').innerHTML = display_text['note'];
+        document.getElementById('note_display').innerHTML = display_text['note'];
     }
 }
 
@@ -48,76 +48,26 @@ network.on('click', function(params) {
     }
 });
 
-function add_node() {
-    var node = prompt('Please enter a label for your new node.', 'Label goes here.');
-    if(!network.body.data.nodes.get(node)) {
-        network.body.data.nodes.add([{ id: node, label: node }]);
-     }
-     call_api('add_node', { label: node, type: 'entity' })
-}
-
-function remove_node() {
-    if (selectedNode == '') {
-            return;
-    }
-    network.body.data.nodes.remove(selectedNode);
-    call_api('remove_node', { label: selectedNode });
-}
-
-function add_edge() {
-    var node1 = prompt('Please enter first node.');
-    var node2 = prompt('Please enter second node.');
-    var edge_label = prompt('Please enter edge label.');
-    if(!network.body.data.edges.get(node1 + '/' + node2) &&
-       !network.body.data.edges.get(node2 + '/' + node1)) {
-       network.body.data.edges.add([{ from: node1, to: node2, id: node1 + '/' + node2, label: edge_label }]);
-       call_api('add_edge', { source: node1, sink: node2, label: edge_label, type: 'semantic' })
-    }
-}
-
-function remove_edge() {
-    if (selectedEdge == '') {
-            return;
-    }
-    network.body.data.edges.remove(selectedEdge);
-    pair = String(selectedEdge).split('/')
-    call_api('remove_edge', { source: pair[0], sink: pair[1] })
-}
-
-function add_note() {
-    if (selectedNode == '') {
-            return;
-    }
-    var note = prompt('Please enter a name for your note.');
-    network.body.data.nodes.add([{ id: note, label: note, color: 'red'}]);
-    call_api('add_node', { label: note, type: 'note'  })
-    network.body.data.edges.add([{ from: note, to: selectedNode, id: note + '/' + selectedNode, label: '--->' }]);
-    call_api('add_edge', { source: note, sink: selectedNode, label: '--->', type: 'note' })
-    update_display_text(note)
-}
-
-function add_parent_child() {
-    var parent = prompt('Please enter parent node.');
-    var child = prompt('Please enter node node.');
-    if(!network.body.data.edges.get(child + '/' + parent) &&
-       !network.body.data.edges.get(parent + '/' + child)) {
-       network.body.data.edges.add([{ from: child, to: parent, id: child + '/' + parent, label: '--->' }]);
-       call_api('add_edge', { source: child, sink: parent, label: '--->', type: 'hierarchical' })
-    }
-}
 
 function restore_graph(data) {
     for (i = 0; i < data['nodes'].length; i++) {
         node = data['nodes'][i]
         if (node[1] == 'entity') {
-            network.body.data.nodes.add([{ id: node[0], label: node[0] }]);
+            network.body.data.nodes.add([{ id: node[0],
+                                           label: node[0] }]);
         } else {
-            network.body.data.nodes.add([{ id: node[0], label: node[0], color: 'red' }]);
+            network.body.data.nodes.add([{ id: node[0],
+                                           label: node[0],
+                                           color: 'red' }]);
         }
     }
     for (j = 0; j < data['edges'].length; j++) {
         edge = data['edges'][j]
-        network.body.data.edges.add([{ from: edge[0], to: edge[1], id: edge[0] + '/' + edge[1], label: edge[2] }]);
+        network.body.data.edges.add([{ from: edge[0],
+                                       to: edge[1],
+                                       id: edge[0] + '/' + edge[1],
+                                       label: edge[2],
+                                       arrows: 'to' }]);
     }
 }
 
@@ -157,6 +107,90 @@ function hide_node() {
         return;
     }
     network.body.data.nodes.remove(selectedNode);
+}
+
+function add_node() {
+    var node = prompt('Please enter a label for your new node.', 'Label goes here.');
+    if(!network.body.data.nodes.get(node)) {
+        network.body.data.nodes.add([{ id: node,
+                                       label: node }]);
+     }
+     call_api('add_node', { label: node,
+                            type: 'entity' })
+}
+
+function remove_node() {
+    if (selectedNode == '') {
+            return;
+    }
+    network.body.data.nodes.remove(selectedNode);
+    call_api('remove_node', { label: selectedNode });
+}
+
+function add_edge() {
+    var node1 = prompt('Please enter first node.');
+    var node2 = prompt('Please enter second node.');
+    var edge_label = prompt('Please enter edge label.');
+    if(!network.body.data.edges.get(node1 + '/' + node2) &&
+       !network.body.data.edges.get(node2 + '/' + node1)) {
+       network.body.data.edges.add([{ from: node1,
+                                      to: node2,
+                                      id: node1 + '/' + node2,
+                                      label: edge_label,
+                                      arrows: 'to' }]);
+       call_api('add_edge', { source: node1,
+                              sink: node2,
+                              label: edge_label,
+                              type: 'semantic' })
+    }
+}
+
+function remove_edge() {
+    if (selectedEdge == '') {
+            return;
+    }
+    network.body.data.edges.remove(selectedEdge);
+    pair = String(selectedEdge).split('/')
+    call_api('remove_edge', { source: pair[0], sink: pair[1] })
+}
+
+function add_note() {
+    if (selectedNode == '') {
+            return;
+    }
+    var note = prompt('Please enter a name for your note.');
+    network.body.data.nodes.add([{ id: note,
+                                   label: note,
+                                   color: 'red'}]);
+    call_api('add_node', { label: note,
+                           type: 'note'  })
+    network.body.data.edges.add([{ from: note,
+                                   to: selectedNode,
+                                   id: note + '/' + selectedNode,
+                                   label: '--->',
+                                   arrows: 'to' }]);
+    call_api('add_edge', { source: note,
+                           sink: selectedNode,
+                           label: '--->',
+                           type: 'note' })
+    update_display_text(note)
+}
+
+function add_parent_child() {
+    var parent = prompt('Please enter parent node.');
+    var child = prompt('Please enter node node.');
+    if(!network.body.data.edges.get(child + '/' + parent) &&
+       !network.body.data.edges.get(parent + '/' + child)) {
+       network.body.data.edges.add([{ from: child,
+                                      to: parent,
+                                      id: child + '/' + parent,
+                                      label: '--->',
+                                      arrows: 'to' }]);
+       call_api('add_edge', { source: child,
+                              sink: parent,
+                              label: '--->',
+                              type: 'hierarchical' })
+    }
 }
 
 initialize();
